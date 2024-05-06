0281a8e01abceee5872498e20119eb347189f822
mamaklangto <94784271+mamaklangto@users.noreply.github.com>
2024-05-06 14:09:07 +0800

    first commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500">
+  <h1 className="text-6xl font-bold text-white">
+    Hello World!
+  </h1>
+</div>
+
+    
+  );
+}
